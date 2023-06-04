ab42b29dcea1e8b82012eb1a6648aa45b5b89d24
shu <shangshu1024@gmail.com>
2023-06-03 21:29:57 -0400

    added NavBar, SideBar, Products Page

---
+++ server/routes/client.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getProducts } from "../controller/client.js";
+const router = express.Router();
+
+router.get("/products", getProducts);
+
+export default router;
