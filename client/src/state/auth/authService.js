4ff6d83699ac8dfee2ea1351c189af9113980307
shu <shangshu1024@gmail.com>
2024-06-24 22:38:02 -0400

    update base url

--- client/src/state/auth/authService.js
+++ client/src/state/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "/api/users/";
-axios.defaults.baseURL = "http://localhost:5001";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
